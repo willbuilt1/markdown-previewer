e939b2ac5dd73fba5432b77d365022e6e8166f37
Will <wfoster12@gmail.com>
2020-04-16 23:28:43 +0100

    Input and first output

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
+import DOMPurify from 'dompurify';
 
 function App() {
+  const [editor, setEditor] = React.useState('Look at me!!!')
+  const onEditorChange = (event) => {
+    setEditor(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea name="editor" id="editor" cols="30" rows="10" value={editor} onChange = {onEditorChange}></textarea>
+      <Preview output={editor}/>
     </div>
   );
+} 
+
+function Preview({output}){
+
+  // const cleanMarkdown = () => DOMPurify.sanitize('<h1>Welcome to my React Markdown Previewer!</h1>')
+
+  return(
+  <div id="preview">{marked(output)}</div>
+  )
 }
 
 export default App;
