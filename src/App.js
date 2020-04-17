05c1433026b5305acb69cd55073b3ae0ace83e1e
Will <wfoster12@gmail.com>
2020-04-17 09:00:17 +0100

    Finished logic passing all tests

--- src/App.js
+++ src/App.js
@@ -4,7 +4,53 @@ import marked from 'marked';
 import DOMPurify from 'dompurify';
 
 function App() {
-  const [editor, setEditor] = React.useState('Look at me!!!')
+  const intialText = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+  
+  ![React Logo w/ Text](https://goo.gl/Umyytc)
+  `
+  const [editor, setEditor] = React.useState(intialText)
   const onEditorChange = (event) => {
     setEditor(event.target.value)
   }
@@ -19,9 +65,15 @@ function App() {
 function Preview({output}){
 
   // const cleanMarkdown = () => DOMPurify.sanitize('<h1>Welcome to my React Markdown Previewer!</h1>')
+  marked.setOptions({
+    breaks: true,
+  })
 
+  function createMarkup() {
+    return {__html: marked(output)};
+  }
   return(
-  <div id="preview">{marked(output)}</div>
+  <div id="preview" dangerouslySetInnerHTML={createMarkup()}></div>
   )
 }
 
